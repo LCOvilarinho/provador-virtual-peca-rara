89423f1794cc5d362f2e3263a0250f479ebc7546
Luiz Claudio <lzcldoliveira@gmail.com>
2026-01-22 10:04:41 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'  // Caminhos relativos: corrige tela preta em deploy
+  base: './'   // <--- isso corrige a maioria das telas pretas
 })
